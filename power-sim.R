9b8262c952cece8622d9895afea35a8c2f76763f
Johannes Schwenke <johannes.schwenke@gmail.com>
2025-04-11 10:37:18 +0200

    - added some comments

--- power-sim.R
+++ power-sim.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(marginaleffects)
 
 # In the first simulation I implemented the acccuracy of each coder is the only
-# random varaible. The underlying accuracies of each coder, the llm, and hoe
+# random varaible. The underlying accuracies of each coder, the llm, and the
 # many reports each coder extract is the same in each simulation. Consinder 2nd
 # version of simulation for that.
 
@@ -11,16 +11,11 @@ library(marginaleffects)
 # because I'm stupid and did no assign a difficult parameter for each report.
 # This messed up the clustering formula.
 
-# TODO: Would like to make performance for LLM and humans different for each
-# report (baiscally a random slope). Separate, but slightly negatively
-# correlated difficulty for humans and llm each.
-
-
 # Null Hypthesis is true, LLM is actually slightly worse than humans.
 
 set.seed(2026)
 
-p_llm   <- 0.90 # Just more than 5% worse. How many times wouuld we say that llm is not more than 5% worse, if it actually is?
+llm_skill   <- 0.95 # Just more than 5% worse. How many times wouuld we say that llm is not more than 5% worse, if it actually is?
 
 results <- data.frame()
 results.ml <- data.frame()
@@ -31,13 +26,28 @@ n_human_coders_per_report <- 2 # Number of humans coding each report
 sd_difficulty <- 0.5 # Standard deviation for report difficulty (logit scale)
 correlation_difficulty <- -0.2 # Correlation between human and LLM difficulty
 
+# NOTE: As the inherent human skill is very very close to one (see blow),
+# increasing the SD of the difficulty (this is on the logit scale), will cause
+# the overall accuracy to go down. Because large positive values of the
+# difficulty will only increase the accuracy on the probability scale from e.g.,
+# 0.95 to 0.98, but large negative values will decrease it from 0.95 to much
+# lower values. It's linear on the logit scale, but not linear on the
+# probability scale.
+
+
 n_human_coders_total <- 6 
-human_accuracies <- c(0.95, 0.95, 0.94, 0.93, 0.96, 0.97) #arithmetric mean = 0.95
+
+# human skill and llm skill refer to the inate ability of the human / llm
+# extractor this will be combined with the difficulty of the report to come up
+# with a probability of a correct answer. From this bernoulli distribution will
+# we will randomly draw in each iteration of the simulation
+
+human_skill <- c(0.95, 0.95, 0.94, 0.93, 0.96, 0.97) #arithmetric mean = 0.95
 
 non_inferiority_margin <- 0.05
 equivalence_bounds <- c(-non_inferiority_margin, Inf) # Test H1: LLM - Human > -0.05
 alpha <- 0.05
-n_reports <- 500
+n_reports <- 300
 
 human_coder_ids <- paste0("human_", 1:n_human_coders_total)
 report_ids <- paste0("report_", 1:n_reports)
@@ -46,17 +56,21 @@ llm_id <- "llm"
 # --- Pre-calculate Lookups ---
 extractor_lookup <- tibble(
   extractor_id = c(human_coder_ids, llm_id),
-  accuracy_p_baseline = c(human_accuracies, p_llm),
-  logit_accuracy_baseline = qlogis(accuracy_p_baseline),
+  skill = c(human_skill, llm_skill),
+  logit_skill = qlogis(skill),
   is_llm = c(rep(0, n_human_coders_total), 1)
 )
 
 # --- Generate FIXED Report Difficulties (Outside Loop) ---
+
 message("Generating fixed report difficulties...")
 report_difficulty_lookup <- tibble(
   report_id = report_ids,
   report_difficulty_logit = rnorm(n_reports, 0, sd_difficulty)
 ) |> 
+  
+  # I'm adding a slight negative correlation between reports for humans and llm,
+  # i.e., reports that are hard for llm are easier for humans and vice versa
   mutate(llm_difficulty_logit = 
            report_difficulty_logit*-correlation_difficulty + 
            rnorm(n_reports, 0, sd_difficulty))
@@ -75,34 +89,39 @@ for (j in 1:n_reports) {
 sim_data_assigned <- bind_rows(sim_data_list_assigned)
 
 # --- Calculate FIXED Actual Probabilities (Outside Loop) ---
+
 # This integrates the report difficult and the accuracy of each extractor
 message("Calculating fixed actual probabilities...")
 sim_setup_data <- sim_data_assigned |>
   left_join(report_difficulty_lookup, by = "report_id") |>
   mutate(
     # Calculate actual probability for this extractor on this report
-    logit_p_human = logit_accuracy_baseline + report_difficulty_logit,
-    logit_p_llm = logit_accuracy_baseline + llm_difficulty_logit,
+    logit_p_human = logit_skill + report_difficulty_logit,
+    logit_llm_skill = logit_skill + llm_difficulty_logit,
     p_human = plogis(logit_p_human),
-    p_llm = plogis(logit_p_llm),
+    p_llm = plogis(logit_llm_skill),
     p_actual = if_else(is_llm == 0, p_human, p_llm)
   )
 
 # --- Calculate the SINGLE FIXED True Average Difference (Outside Loop) ---
-message("Calculating the single true average difference...")
-avg_p_actual_llm_fixed <- mean(sim_setup_data$p_llm[sim_setup_data$is_llm == 1])
-avg_p_actual_human_fixed <- mean(sim_setup_data$p_human[sim_setup_data$is_llm == 0])
-true_difference_fixed <- avg_p_actual_llm_fixed - avg_p_actual_human_fixed
 
+avg_p_actual_llm_fixed <- mean(sim_setup_data$p_llm[sim_setup_data$is_llm == 1])
+message(paste0("The average llm accuracy is ", round(avg_p_actual_llm_fixed, digits = 3), " with an SD of ", round(sd(sim_setup_data$p_llm[sim_setup_data$is_llm == 1]), digits = 3)))
+
+avg_p_actual_human_fixed <- mean(sim_setup_data$p_human[sim_setup_data$is_llm == 0])
+message(paste0("The (weighted) average human accuracy is ", round(avg_p_actual_human_fixed, digits = 3), " with an SD of ", round(sd(sim_setup_data$p_human[sim_setup_data$is_llm == 0]), digits = 3)))
+
+true_difference_fixed <- avg_p_actual_llm_fixed - avg_p_actual_human_fixed
 message(paste("Fixed True Average Difference (LLM - Human):", round(true_difference_fixed, 4)))
-# Not exactly 0.5, because not all humans extract same proportion of reports. 
+
+# Not exactly the values specified above, because not all humans extract same proportion of reports. 
 
 sim_data_list <- vector("list", n_reports)
 
 results <- data.frame()
 results_m <- data.frame()
 
-for(i in 1:100){
+for(i in 1:200){
   message(paste0("Simulation ", i))
   
   sim_data_long <- sim_setup_data |> 
@@ -129,36 +148,21 @@ for(i in 1:100){
     type = "response",
     comparison = "difference",
     vcov = ~report_id,
-    #df = n_human_coders_total, # not -1 because llm is also cluster
-    equivalence = equivalence_bounds
+    equivalence = equivalence_bounds # this calculates the non-inferioty p-value (see bound set at the top)
   )
-  
-  # comparison_m <- avg_comparisons(
-  #   model_m_fit,
-  #   variables = "is_llm",
-  #   type = "response",
-  #   comparison = "difference",
-  #   equivalence = equivalence_bounds,
-  #   re.form = NULL
-  # )
   
   results <- bind_rows(
     comparison |> data.frame() |> select(estimate, conf.low, conf.high, p.value, p.value.noninf), 
     results)
-  
-  # results_m <- bind_rows(
-  #   comparison_m |> data.frame() |> select(estimate, conf.low, conf.high, p.value, p.value.noninf), 
-  #   results)
-  
 }
 
-# Calculate Type I error rate
+# Calculate Type I error rate / power if the llm is as good as the human
 type_i_error_rate <- mean(results$p.value.noninf < 0.05)
 type_i_error_rate
 
 # Not sure why the two don't match
 cbind(results, true_difference_sim) |> 
-  mutate(type_i_error = if_else(conf.low > true_difference_sim, 1, 0)) |> 
+  mutate(type_i_error = if_else(conf.low > -0.05, 1, 0)) |> 
   pull(type_i_error) |> mean()
 
 cbind(results, true_difference_sim) |> 
@@ -178,275 +182,3 @@ cbind(results, true_difference_sim) |>
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank()
   )
-
-# cbind(results_m[1:100, ], true_difference_sim) |> 
-#   mutate(sim = row_number()) |> 
-#   data.frame() |> 
-#   ggplot() +
-#   geom_point(aes(x = sim, y = estimate)) +
-#   geom_errorbar(aes(x = sim, y = estimate, ymin = conf.low, ymax = conf.high)) + 
-#   geom_hline(yintercept = -non_inferiority_margin, color = "red") + #non-inferiority margin
-#   geom_point(aes(y = true_difference_sim, x = sim), color = "blue", size = 2) + #true difference
-#   theme_light() +
-#   labs(
-#     x = "Simulations",
-#     y  = "Difference (LLM - Humans)"
-#   ) +
-#   theme(
-#     axis.text.x = element_blank(),
-#     axis.ticks.x = element_blank()
-#   )
-
-
-
-
-
-
-
-
-
-
-
-
-### IGNORE FOR THE TIME BEING #########
-
-
-# --- Simulation Parameters ---
-
-# --- Parameters for Data Generation ---
-# Number of human coders involved in the study
-n_human_coders_total <- 10 # Total pool of human coders
-n_human_coders_per_report <- 2 # Number of humans coding each report
-
-# Beta distribution shape parameters for human coder accuracy
-# Higher shape1 relative to shape2 means higher mean accuracy.
-# Higher sum (shape1+shape2) means lower variance.
-beta_shape1 <- 80
-beta_shape2 <- 4.204
-
-# Check mean: beta_shape1 / (beta_shape1 + beta_shape2) -> ~0.95
-
-# LLM's true accuracy
-p_llm   <- 0.95 # Assumed true accuracy for LLM
-
-# --- Hypothesis Testing Parameters ---
-non_inferiority_margin <- 0.05
-equivalence_bounds <- c(-non_inferiority_margin, Inf) # Test H1: LLM - Human > -0.03
-alpha <- 0.05
-n_reports <- 500
-
-human_coder_ids <- paste0("human_", 1:n_human_coders_total)
-report_ids <- paste0("report_", 1:n_reports)
-llm_id <- "llm"
-
-set.seed(1234)
-
-results <- data.frame()
-
-for(i in 1:100){
-message(paste0("Simulation ", i))
-  
-human_accuracies <- rbeta(n_human_coders_total, shape1 = beta_shape1, shape2 = beta_shape2)
-
-extractor_lookup <- tibble(
-    extractor_id = c(human_coder_ids, llm_id),
-    accuracy_p = c(human_accuracies, p_llm),
-    is_llm = c(rep(0, n_human_coders_total), 1)
-  )
-
-sim_data_list <- vector("list", n_reports)
-
-for (i in 1:n_reports) {
-  selected_human_ids <- sample(human_coder_ids, n_human_coders_per_report, replace = FALSE)
-  report_extractors <- c(selected_human_ids, llm_id)
-  report_extractor_info <- extractor_lookup |> filter(extractor_id %in% report_extractors)
-  report_data <- report_extractor_info |>
-    mutate(
-      report_id = report_ids[i],
-      correct = rbinom(n(), 1, accuracy_p)
-    ) |>
-    select(report_id, extractor_id, is_llm, correct) # Removed accuracy_p
-  sim_data_list[[i]] <- report_data
-}
-
-sim_data_long <- bind_rows(sim_data_list)
-  
-# Fit Simple GLM Model (ignoring clustering structure here)
-model_fit <- glm(correct ~ is_llm, data = sim_data_long, family = binomial(link = "logit"))
-
-# Calculate difference using marginal effects. Uses delta method and cluster robust standard errors.
-comparison <- avg_comparisons(
-      model_fit,
-      variables = "is_llm",
-      type = "response",
-      # Specify two-way clustering formula for vcov
-      vcov = ~report_id + extractor_id,
-      equivalence = equivalence_bounds
-    )
-
-results <- bind_rows(
-  comparison |> data.frame() |> select(estimate, conf.low, conf.high, p.value, p.value.noninf), 
-  results)
-}
-
-#Calculate Power
-power <- mean(results$p.value.noninf < 0.05)
-
-
-results |> 
-  mutate(sim = row_number()) |> 
-  data.frame() |> 
-  ggplot(aes(x = sim, y = estimate)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high)) + 
-  theme_light() +
-  labs(
-    x = "Simulations",
-    y  = "Difference (LLM - Humans)"
-  ) +
-  theme(
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank()
-  )
-
-
-#### Repeat again, when LLM is actually worse to get Tupe I error rate
-# Everything stays the same, except for llm-accuracy
-
-p_llm   <- 0.899 # Just more than 5% worse. How many times wouuld we say that llm is not more than 5% worse, if it actually is?
-
-set.seed(2024)
-
-results <- data.frame()
-results.ml <- data.frame()
-true_difference_sim <- numeric()
-
-for(i in 1:50){
-  message(paste0("Simulation ", i))
-  
-  human_accuracies <- rbeta(n_human_coders_total, shape1 = beta_shape1, shape2 = beta_shape2)
-  
-  extractor_lookup <- tibble(
-    extractor_id = c(human_coder_ids, llm_id),
-    accuracy_p = c(human_accuracies, p_llm),
-    is_llm = c(rep(0, n_human_coders_total), 1)
-  )
-  
-  sim_data_list <- vector("list", n_reports)
-  
-  for (j in 1:n_reports) {
-    selected_human_ids <- sample(human_coder_ids, n_human_coders_per_report, replace = FALSE)
-    report_extractors <- c(selected_human_ids, llm_id)
-    report_extractor_info <- extractor_lookup |> filter(extractor_id %in% report_extractors)
-    report_data <- report_extractor_info |>
-      mutate(
-        report_id = report_ids[j],
-        correct = rbinom(n(), 1, accuracy_p)
-      ) |>
-      select(report_id, extractor_id, is_llm, correct, accuracy_p)
-    sim_data_list[[j]] <- report_data
-  }
-  
-  sim_data_long <- bind_rows(sim_data_list)
-  
-  human_data_sim <- sim_data_long |> filter(is_llm == 0)
-  true_mean_human_accuracy_sim <- sum(human_data_sim$accuracy_p) / nrow(human_data_sim)
-  
-  true_difference_sim[i] <- p_llm - true_mean_human_accuracy_sim
-  
-  
-  # Fit Simple GLM Model (ignoring clustering structure here)
-  model_fit <- glm(correct ~ is_llm, data = sim_data_long, family = binomial(link = "logit"))
-  # ml.model_fit <- glmer(correct ~ is_llm + (is_llm|report_id) + (1|extractor_id), data = sim_data_long, family = binomial(link = "logit"))
-  
-  # Calculate difference using marginal effects. Uses delta method and cluster robust standard errors.
-  comparison <- avg_comparisons(
-    model_fit,
-    variables = "is_llm",
-    type = "response",
-    # Specify two-way clustering formula for vcov
-    vcov = ~report_id + extractor_id,
-    df = 10 - 1,
-    equivalence = equivalence_bounds
-  )
-  
-  # ml.comparison <- avg_comparisons(
-  #   ml.model_fit,
-  #   variables = "is_llm",
-  #   type = "response",
-  #   equivalence = equivalence_bounds,
-  #   re.form = NULL
-  # )
-  
-  results <- bind_rows(
-    comparison |> data.frame() |> select(estimate, conf.low, conf.high, p.value, p.value.noninf), 
-    results)
-  
-  # results.ml <- bind_rows(
-  #   ml.comparison |> data.frame() |> select(estimate, conf.low, conf.high, p.value, p.value.noninf), 
-  #   results)
-  
-}
-
-#Calculate Power
-type_i_error_rate <- mean(results$p.value.noninf < 0.05)
-type_i_error_rate
-
-results |> 
-  mutate(sim = row_number()) |> 
-  data.frame() |> 
-  ggplot(aes(x = sim, y = estimate)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high)) + 
-  theme_light() +
-  labs(
-    x = "Simulations",
-    y  = "Difference (LLM - Humans)"
-  ) +
-  theme(
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank()
-  )
-
-cbind(results, true_difference_sim) |> 
-  mutate(sim = row_number()) |> 
-  data.frame() |> 
-  ggplot() +
-  geom_point(aes(x = sim, y = estimate)) +
-  geom_errorbar(aes(x = sim, y = estimate, ymin = conf.low, ymax = conf.high)) + 
-  geom_point(aes(y = true_difference_sim, x = sim), color = "blue", size = 2) + #true difference
-  theme_light() +
-  labs(
-    x = "Simulations",
-    y  = "Difference (LLM - Humans)"
-  ) +
-  theme(
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank()
-  )
-
-cbind(results, true_difference_sim) |> 
-  mutate(type_i_error = if_else(conf.low > true_difference_sim, 1, 0)) |> 
-  pull(type_i_error) |> mean()
-
-# results.m |> 
-#   mutate(sim = row_number()) |> 
-#   data.frame() |> 
-#   ggplot(aes(x = sim, y = estimate)) +
-#   geom_point() +
-#   geom_errorbar(aes(ymin = conf.low, ymax = conf.high)) + 
-#   theme_light() +
-#   labs(
-#     x = "Simulations",
-#     y  = "Difference (LLM - Humans)"
-#   ) +
-#   theme(
-#     axis.text.x = element_blank(),
-#     axis.ticks.x = element_blank()
-#   )
-
-
-### Test if it works better if I fix the accuracies of the participants to a
-### certain value and the proporion of reports they work on. Let's look at equal
-### proportions first.
-
